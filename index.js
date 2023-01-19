f4bd3fddeced93af869a53b8d3557879d97050df
Palesa Mpe <palesa@bbd.co.za>
2023-01-19 12:07:23 +0200

    updated part 1

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let userName,loverName="";
 
-document.querySelector(".nameInput").addEventListener("onChange",function(e){
-console.log(e);
-});
+
+
 
 document.querySelector(".generate").addEventListener("click",function(){
-    console.log("clicked");
+     userName = document.querySelectorAll(".nameInput")[0].value;
+     loverName = document.querySelectorAll(".nameInput")[1].value;
 
     let generatedValue = Math.floor(Math.random()*101);
 
-    document.querySelector(".generatedValue").innerHTML = generatedValue+"%";
+    document.querySelector("#display").innerHTML = `${userName} and ${loverName}, your love score is:`;
     document.querySelector(".generatedValue").innerHTML = generatedValue+"%";
     console.log(generatedValue);
 })
